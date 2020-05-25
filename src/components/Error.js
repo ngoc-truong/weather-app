ef3317fbda2768dde33c43cc5970861e320e6b38
Ngoc Truong <ngoc.truong@posteo.de>
2020-05-25 20:13:51 +0200

    Change error message

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from "react";
 const Error = () => {
     return (
         <div id="error" className="hide">
-            <h1 className="error">Sorry, I couldn't find the city :(.</h1>
+            <h1 className="error">Sorry, I couldn't find the city :(</h1>
         </div>
     )
 }
